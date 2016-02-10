3119642172304bcdf5f1a04e6d706b6717632dd5
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-02-10 14:43:43 -0500

    django pythia hasher issue resolved by setting everythign to string

--- pythia/hashers.py
+++ pythia/hashers.py
@@ -3,15 +3,48 @@ import safeid
 from django.conf import settings
 from django.contrib.auth.hashers import BasePasswordHasher
 
-
 class PythiaSafeIDHasher(BasePasswordHasher):
     algorithm = "pythia"
 
     def encode(self, password, salt):
         w, t, z, p = safeid.new(password)
-        return  "%s$%s$%s$%s$%s" %  (self.algorithm, w, t, z, p)
+        
+        print "password: %s" % type(password)
+        print "w: %s" % type(w)
+        print "t: %s" % type(t)
+        print "z: %s" % type(z)
+        print "p: %s" % type(p)
+        
+        encoded = "%s$%s$%s$%s$%s" % (self.algorithm, w, t, z, p)
+        print "encoded: %s" % type(encoded)
+        
+        return "%s$%s$%s$%s$%s" % (self.algorithm, w, t, z, p)
 
     def verify(self, password, encoded):
-        algorith, w, t, z, p = encoded.split('$', 5)
-        return safeid.check(password, w, t, z, p)
         
+        print "encoded: %s" % type(encoded)
+        algorith, w, t, z, p = encoded.split('$', 5)
+    
+        print "password: %s" % type(password)
+        print "w: %s" % type(w)
+        print "t: %s" % type(t)
+        print "z: %s" % type(z)
+        print "p: %s" % type(p)
+        
+        
+        password = str(password)
+        w = str(w)
+        t = str(t)
+        z = str(z)
+        p = str(p)
+        
+        print "password: %s" % type(password)
+        print "w: %s" % type(w)
+        print "t: %s" % type(t)
+        print "z: %s" % type(z)
+        print "p: %s" % type(p)
+        
+        
+    
+        # print 'password: %s\nw: %s\nt: %s\nz: %s\np: %s' % (password, w, t, z, p)
+        return safeid.check(password, w, t, z, p)
--- pythia/safeid.py
+++ pythia/safeid.py
@@ -4,6 +4,7 @@ SafeID interacts with a Pythia PRF service to protect and verify passwords.
 """
 import argparse, json, sys
 from common import *
+import urllib
 from httpJson import ServiceException, fetch, extract
 from pyrelic import vpop
 
@@ -23,140 +24,138 @@ check 'pw' 'JSON'   Checks a given passphrase against an existing
 
 
 def new(password, server=defaultServer, clientId=None):
-	"""
-	Encrypts a new @password by interacting with the Pythia service.
-	@clientId: If specified, this value is used. If omitted, a suitable 
-	           value is selected.
+    """
+    Encrypts a new @password by interacting with the Pythia service.
+    @clientId: If specified, this value is used. If omitted, a suitable 
+                value is selected.
 
-	@returns a tuple of required values to verify the password: (w,t,z,p) 
-	 where:
-		@w: client ID (key selector)
-		@t: tweak (randomly generated user ID)
-		@z: protected passwords
-		@p: server public key bound to clientId - used to verify future
-		    proofs from this server.
-	"""
-	# Set the client ID
-	if not clientId:
-		w = secureRandom()
-	else:
-		w = clientId
+    @returns a tuple of required values to verify the password: (w,t,z,p) 
+        where:
+        @w: client ID (key selector)
+        @t: tweak (randomly generated user ID)
+        @z: protected passwords
+        @p: server public key bound to clientId - used to verify future
+            proofs from this server.
+    """
+    # Set the client ID
+    if not clientId:
+        w = secureRandom()
+    else:
+        w = clientId
 
-	# Generate a random tweak t
-	t = secureRandom()
-	z,p = query(password, w, t, server)
-	return w, t, z, p
+    # Generate a random tweak t
+    t = secureRandom()
+    z,p = query(password, w, t, server)
+    return w, t, z, p
 
 
 def check(password, w, t, z, p, server=defaultServer):
-	"""
-	Checks an existing @password against the Pythia server using the 
-	values (w,t,z,p).
-	@returns: True if the password passes authentication; False otherwise.
-	"""
-	zPrime,_ = query(password, w, t, previousPubkey=p, server=server)
-	return z == zPrime
-
-
+    """
+    Checks an existing @password against the Pythia server using 
+    values (w,t,z,p).
+    @returns: True if the password passes authentication; False otherwise.he 	
+    """
+    zPrime,_ = query(password, w, t, previousPubkey=p, server=server)
+    return z == zPrime	
+	
+	
 def query(password, w, t, server=defaultServer, previousPubkey=None):
-	"""
-	Queries the a Pythia PRF service and verifies the server's ZKP.
-	@returns (z,p) where: @z is the encrypted password and @p is the
-		server's pubkey bound to clientId
+    """
+    Queries the a Pythia PRF service and verifies the server's ZKP.
+    @returns (z,p) where: @z is the encrypted password and @p is the
+    server's pubkey bound to clientId
+    
+    Raises an exception if there are any problems interacting with the service
+    or if the server's ZKP fails verification.
+    """
+    # Blind the password
+    r,x = vpop.blind(password)
+    xSerialized = vpop.wrap(x)
+    
+    response = fetch(queryUrlTemplate.format(server,w,t,xSerialized))
 
-	Raises an exception if there are any problems interacting with the service
-		or if the server's ZKP fails verification.
-	"""
-	# Blind the password
-	r,x = vpop.blind(password)
-	xSerialized = vpop.wrap(x)
+    # Grab the required fields from the response.
+    p,y,c,u = extract(response, ["p","y","c","u"])
 
-	# Query the service via HTTP(S) GET
-	response = fetch(queryUrlTemplate.format(server,w,t,xSerialized))
+    # Check the pubkey
+    if previousPubkey and previousPubkey != p:
+        raise Exception("Server-provided pubkey doesn't match previous pubkey.")
 
-	# Grab the required fields from the response.
-	p,y,c,u = extract(response, ["p","y","c","u"])
+    # Deserialize the response fields
+    p,y,c,u = (vpop.unwrapP(p), vpop.unwrapY(y), vpop.unwrapC(c), vpop.unwrapU(u))
 
-	# Check the pubkey
-	if previousPubkey and previousPubkey != p:
-		raise Exception("Server-provided pubkey doesn't match previous pubkey.")
+    pi = (p,c,u)
 
-	# Deserialize the response fields
-	p,y,c,u = (vpop.unwrapP(p), vpop.unwrapY(y), 
-			vpop.unwrapC(c), vpop.unwrapU(u))
+    # Verify the result by checking the proof
+    vpop.verify(x, t, y, pi)
 
-	pi = (p,c,u)
+    # Deblind the result
+    z = vpop.deblind(r,y)
 
-	# Verify the result by checking the proof
-	vpop.verify(x, t, y, pi)
-
-	# Deblind the result
-	z = vpop.deblind(r,y)
-
-	# Return the important fields in serialied form
-	z,p = vpop.wrap(z), vpop.wrap(p)
-	return z,p
+    # Return the important fields in serialied form
+    z,p = vpop.wrap(z), vpop.wrap(p)
+    return z,p
 
 
 def main():
-	"""
-	Run the safeid command line program
-	"""
-	# DEBUG
-	process(sys.argv)
-	return
-	try:
-		process(sys.argv)
-	except Exception as e:
-		print e
+    """
+    Run the safeid command line program
+    """
+    # DEBUG
+    process(sys.argv)
+    return
+    try:
+        process(sys.argv)
+    except Exception as e:
+        print e
 
 
 def process(args):
-	"""
-	Command line interface to SafeID
-	"""
-	# Parse arguments
-	parser = argparse.ArgumentParser(usage=usage)
-	parser.add_argument("COMMAND", choices=["new", "check"])
-	parser.add_argument("passphrase", type=str)
-	parser.add_argument("protectedPassphrase", nargs="?", type=str)
-	parser.add_argument("-s", "--server", default=defaultServer, type=str)
-	a = parser.parse_args(args[1:])
+    """
+    Command line interface to SafeID
+    """
+    # Parse arguments
+    parser = argparse.ArgumentParser(usage=usage)
+    parser.add_argument("COMMAND", choices=["new", "check"])
+    parser.add_argument("passphrase", type=str)
+    parser.add_argument("protectedPassphrase", nargs="?", type=str)
+    parser.add_argument("-s", "--server", default=defaultServer, type=str)
+    a = parser.parse_args(args[1:])
 
-	##
-	# Run the requested command
-	##
+    ##
+    # Run the requested command
+    ##
 
-	# New password
-	if a.COMMAND == "new":
-		print json.dumps(new(a.passphrase, a.server))
+    # New password
+    if a.COMMAND == "new":
+        print json.dumps(new(a.passphrase, a.server))
 
-	# Check existing password
-	elif a.COMMAND == "check" and a.protectedPassphrase:
+    # Check existing password
+    elif a.COMMAND == "check" and a.protectedPassphrase:
 
-		# Parse the encrypted password
-		args = readJson(a.protectedPassphrase)
+        # Parse the encrypted password
+        args = readJson(a.protectedPassphrase)
 
-		# Check the password
-		if check(a.passphrase,*args,server=a.server):
-			print  "Password is authentic"
-		else:
-			print "Invalid password "
+        # Check the password
+        if check(a.passphrase,*args,server=a.server):
+            print  "Password is authentic"
+        else:
+            print "Invalid password "
 
-	else:
-		print "usage: " + usage
+    else:
+        print "usage: " + usage
 
 
 def readJson(text):
-	"""
-	Parses JSON array and returns (w,t,z,p) as strings.
-	"""
-	# Convert all unicode JSON results to strings. ZK proofs fail when
-	# one party uses unicode and the other uses strings.
-	return map(str, json.loads(text))
+    """
+    Parses JSON array and returns (w,t,z,p) as strings.
+    """
+    # Convert all unicode JSON results to strings. ZK proofs fail when
+    # one party uses unicode and the other uses strings.
+    return map(str, json.loads(text))
 
 
 # Run!
 if __name__ == "__main__":
-	main()
+    main()
 
